e1c8decf7871ed91b8ac7a00444c0d8a9021c9a8
test <mocnygaznepido@gmail.com>
2026-09-14 12:42:07 +0200

    losowanie liczby

--- lotto2_poniedzialek/Program.cs
+++ lotto2_poniedzialek/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Random random  = new Random();
+
+var liczba = 6; //typ rozpoznany po pierwszym uzyciu
+int liczbaWylosowana = random.Next(1, 50);
+int liczbaWylosowana2 = random.Next( 50);
+//przeciążenie metody Next
+//przeciążenie metody -> ta sama nazwa, różna liczba lub typ parametry
+Console.WriteLine(liczbaWylosowana);
+Console.ReadLine();
